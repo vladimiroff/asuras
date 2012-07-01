d5974f62be8e5dc37b5e14fac70a634a3c138b7c
Kiril Vladimirov <kiril@vladimiroff.org>
2012-07-01 20:20:35 +0300

    Add main loop

---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import pygame
+
+
+class Game:
+    fields = []
+    pieces = {}
+    sprites = pygame.sprite.Group()
+
+    def main(self, screen):
+        ''' The main loop '''
+        clock = pygame.time.Clock()
+        # wait David to figure out how to initialize the map
+
+        while 1:
+            dt = clock.tick(45)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+
+            self.sprites.update(dt)
+            screen.fill((239, 237, 236))
+            self.sprites.draw(screen)
+            pygame.display.flip()
+
+        pass
+
+if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Asuras")
+    Game().main(screen)
