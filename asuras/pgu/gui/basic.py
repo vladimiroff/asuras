5dda8f6c28d027913ffe6ac2f4a9bdb7b832e454
Kiril Vladimirov <kiril@vladimiroff.org>
2012-08-18 03:23:27 +0300

    Refactoring in pgu.gui.basic
    My eyes are still bleeding...

--- asuras/pgu/gui/basic.py
+++ asuras/pgu/gui/basic.py
@@ -1,60 +1,49 @@
 """Basic widgets and various utility functions.
 """
 
-###########
-# Imports #
-###########
-
 import pygame
 
 from pgu.gui import events, widget
 from pgu.gui.errors import PguError
 
-#############
-# Functions #
-#############
 
-# Turns a descriptive string or a tuple into a pygame color
 def parse_color(desc):
+    ''' Turns a descriptive string or a tuple into a pygame color
+    Because of a bug in pygame 1.8.1 we need to explicitly define the
+    alpha value otherwise it will default to transparent.
+    '''
     if (is_color(desc)):
-        # Already a color
         return desc
-    elif (desc and desc[0] == "#"):
-        # Because of a bug in pygame 1.8.1 we need to explicitly define the 
-        # alpha value otherwise it will default to transparent.
-        if (len(desc) == 7):
+    elif desc and desc[0] == "#":
+        if len(desc) == 7:
             desc += "FF"
     return pygame.Color(desc)
 
-# Determines if the given object is a pygame-compatible color or not
+
 def is_color(col):
-    # In every version of pygame (up to 1.8.1 so far) will interpret
-    # a tuple as a color.
+    ''' Determines if the given object is a pygame-compatible color or not
+    In every version of pygame (up to 1.8.1 so far) will interpret
+    a tuple as a color.
+    '''
     if (type(col) == tuple or type(col) == list):
         return col
     if (hasattr(pygame, "Color") and type(pygame.Color) == type):
-        # This is a recent version of pygame that uses a proper type
-        # instance for colors.
         return (isinstance(col, pygame.Color))
-    # Otherwise, this version of pygame only supports tuple colors
     return False
 
-###########
-# Classes #
-###########
 
 class Spacer(widget.Widget):
     """An invisible space widget."""
 
-    def __init__(self,width,height,**params):
-        params.setdefault('focusable',False)
-        widget.Widget.__init__(self,width=width,height=height,**params)
-        
+    def __init__(self, width, height, **params):
+        params.setdefault('focusable', False)
+        widget.Widget.__init__(self,  width=width, height=height, **params)
+
 
 class Color(widget.Widget):
     """A widget that renders as a solid block of color.
-    
-    Note the color can be changed by setting the 'value' field, and the 
+
+    Note the color can be changed by setting the 'value' field, and the
     widget will automatically be repainted, eg:
 
         c = Color()
@@ -63,16 +52,16 @@ class Color(widget.Widget):
 
     """
 
-    # The pygame Color instance
     _value = None
-    
-    def __init__(self,value=None,**params):
-        params.setdefault('focusable',False)
-        if value != None: params['value']=value
-        widget.Widget.__init__(self,**params)
-    
-    def paint(self,s):
-        if hasattr(self,'value') and is_color(self.value): 
+
+    def __init__(self, value=None, **params):
+        params.setdefault('focusable', False)
+        if value != None:
+            params['value'] = value
+        widget.Widget.__init__(self, **params)
+
+    def paint(self, s):
+        if hasattr(self, 'value') and is_color(self.value):
             s.fill(self.value)
 
     @property
@@ -80,17 +69,15 @@ class Color(widget.Widget):
         return self._value
 
     @value.setter
-    def value(self, val):
-        if (isinstance(val, str)):
-            # Parse the string as a color
-            val = parse_color(val)
-        oldval = self._value
-        self._value = val
-        if (oldval != val):
-            # Emit a change signal
+    def valueue(self,  value):
+        if (isinstance(value,  str)):
+            value = parse_color(value)
+        old_value = self._value
+        self._valueue = value
+        if (old_value != value):
             self.send(events.CHANGE)
             self.repaint()
-    
+
 
 class Label(widget.Widget):
     """A text label widget."""
@@ -102,24 +89,27 @@ class Label(widget.Widget):
         self.style.check("font")
         self.value = value
         self.style.width, self.style.height = self.font.size(self.value)
-    
-    def paint(self,s):
-        """Renders the label onto the given surface in the upper-left corner."""
-        #s.blit(self.font.render(self.value, 1, self.style.color),(0,0))
-        s.blit(self.style.font.render(self.value, 1, self.style.color),(0,0))
+
+    def paint(self, s):
+        """Renders the label onto the given surface in the upper-left corner"""
+        s.blit(self.style.font.render(self.value, 1, self.style.color), (0, 0))
 
     def set_text(self, txt):
-        """Set the text of this label."""
+        """Set the text of this label.
+        Signal to the application that we need
+        to resize this widget is being sent
+        """
         self.value = txt
-        # Signal to the application that we need to resize this widget
         self.chsize()
 
     def set_font(self, font):
-        """Set the font used to render this label. Obsolete: use label.font instead"""
+        """Set the font used to render this label.
+
+        Obsolete: use label.font instead"""
         self.font = font
 
-    def resize(self,width=None,height=None):
-        # Calculate the size of the rendered text
+    def resize(self, width=None, height=None):
+        ''' Calculate the size of the rendered text '''
         (self.style.width, self.style.height) = self.font.size(self.value)
         return (self.style.width, self.style.height)
 
@@ -129,41 +119,40 @@ class Label(widget.Widget):
 
     @font.setter
     def font(self, font):
+        ''' Signal to the application that we need a resize is being sent '''
         self.style.font = font
-        # Signal to the application that we need a resize
         self.chsize()
 
+
 class Image(widget.Widget):
-    """An image widget. The constructor takes a file name or a pygame surface."""
+    '''An image widget.
+    The constructor takes a file name or a pygame surface.
+    '''
 
-    def __init__(self,value,**params):
-        params.setdefault('focusable',False)
-        widget.Widget.__init__(self,**params)
+    def __init__(self, value, **params):
+        params.setdefault('focusable', False)
+        widget.Widget.__init__(self, **params)
 
-        if (not value):
-            raise PguError("Image widget takes a path or pygame surface as first argument")
-
-        if (isinstance(value, str)):
-            # Assume the argument is a path
+        if isinstance(value,  str):
             value = pygame.image.load(value)
             if (not value):
                 raise PguError("Cannot load the image '%s'" % value)
 
-        ow,oh = iw,ih = value.get_width(),value.get_height()
-        sw,sh = self.style.width,self.style.height
-        
-        if sw and not sh:
-            iw,ih = sw,ih*sw/iw
-        elif sh and not sw:
-            iw,ih = iw*sh/ih,sh
-        elif sw and sh:
-            iw,ih = sw,sh
-        
-        if (ow,oh) != (iw,ih):
-            value = pygame.transform.scale(value,(iw,ih))
-        self.style.width,self.style.height = iw,ih
-        self.value = value
-    
-    def paint(self,s):
-        s.blit(self.value,(0,0))
+        initial_width, initial_height = value.get_width(), value.get_height()
+        width, height = initial_width, initial_height
+        style_width, style_height = self.style.width, self.style.height
 
+        if style_width and not style_height:
+            width, height = style_width, height * style_width / width
+        elif style_height and not style_width:
+            width, height = width * style_height / height, style_height
+        elif style_width and style_height:
+            width, height = style_width, style_height
+
+        if (initial_width, initial_height) != (width, height):
+            value = pygame.transform.scale(value, (width, height))
+        self.style.width, self.style.height = width, height
+        self.value = value
+
+    def paint(self, screen):
+        screen.blit(self.value, (0, 0))
